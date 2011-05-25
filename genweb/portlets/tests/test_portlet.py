d0e8560fb6ce03f4e3d837305086c4635404e05d
Víctor Fernández de Alba <sneridagh@sneridagh.(none)>
2011-05-25 16:18:47 +0200

    first commit

---
+++ genweb/portlets/tests/test_portlet.py
@@ -0,0 +1,112 @@
+from zope.component import getUtility, getMultiAdapter
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from genweb.portlets.portlets import rssvideoportlet
+
+from genweb.portlets.portlets.tests.base import TestCase
+
+
+class TestPortlet(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def test_portlet_type_registered(self):
+        portlet = getUtility(
+            IPortletType,
+            name='genweb.portlets.portlets.rssvideoportlet')
+        self.assertEquals(portlet.addview,
+                          'genweb.portlets.portlets.rssvideoportlet')
+
+    def test_interfaces(self):
+        # TODO: Pass any keyword arguments to the Assignment constructor
+        portlet = rssvideoportlet.Assignment()
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_add_view(self):
+        portlet = getUtility(
+            IPortletType,
+            name='genweb.portlets.portlets.rssvideoportlet')
+        mapping = self.portal.restrictedTraverse(
+            '++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        # TODO: Pass a dictionary containing dummy form inputs from the add
+        # form.
+        # Note: if the portlet has a NullAddForm, simply call
+        # addview() instead of the next line.
+        addview.createAndAdd(data={})
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0],
+                                   rssvideoportlet.Assignment))
+
+    def test_invoke_edit_view(self):
+        # NOTE: This test can be removed if the portlet has no edit form
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        mapping['foo'] = rssvideoportlet.Assignment()
+        editview = getMultiAdapter((mapping['foo'], request), name='edit')
+        self.failUnless(isinstance(editview, rssvideoportlet.EditForm))
+
+    def test_obtain_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn',
+                             context=self.portal)
+
+        # TODO: Pass any keyword arguments to the Assignment constructor
+        assignment = rssvideoportlet.Assignment()
+
+        renderer = getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, rssvideoportlet.Renderer))
+
+
+class TestRenderer(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(
+            IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        # TODO: Pass any default keyword arguments to the Assignment
+        # constructor.
+        assignment = assignment or rssvideoportlet.Assignment()
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
+
+    def test_render(self):
+        # TODO: Pass any keyword arguments to the Assignment constructor.
+        r = self.renderer(context=self.portal,
+                          assignment=rssvideoportlet.Assignment())
+        r = r.__of__(self.folder)
+        r.update()
+        #output = r.render()
+        # TODO: Test output
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
+    return suite
