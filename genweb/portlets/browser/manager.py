2006624e5b429e23d3962e8670d0359c61f8e48f
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-11-26 17:04:02 +0100

    New adapter for handling edit portlets manager.

--- genweb/portlets/browser/manager.py
+++ genweb/portlets/browser/manager.py
@@ -3,9 +3,12 @@ from zope.interface import Interface
 from zope.publisher.interfaces.browser import IBrowserView
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.portlets.manager import ColumnPortletManagerRenderer
+from plone.app.portlets.browser.interfaces import IManageContextualPortletsView
+from plone.app.portlets.browser.editmanager import ContextualEditPortletManagerRenderer
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from genweb.portlets.browser.interfaces import IHomepagePortletManager
+
 
 class GenwebPortletRenderer(ColumnPortletManagerRenderer):
     """
@@ -13,3 +16,10 @@ class GenwebPortletRenderer(ColumnPortletManagerRenderer):
     """
     adapts(Interface, IDefaultBrowserLayer, IBrowserView, IHomepagePortletManager)
     template = ViewPageTemplateFile('templates/renderer.pt')
+
+
+class gwContextualEditPortletManagerRenderer(ContextualEditPortletManagerRenderer):
+    """Render a portlet manager in edit mode for contextual portlets"""
+    adapts(Interface, IDefaultBrowserLayer, IManageContextualPortletsView, IHomepagePortletManager)
+
+    template = ViewPageTemplateFile('templates/edit-manager-contextual.pt')
