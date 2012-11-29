e185287ca11570dc3c24a54d1218ed9251486436
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-11-29 18:12:35 +0100

    Update of the portlet editor, added IAnnotatable behavior to the PortletManager and JS associated (WIP)

--- genweb/portlets/browser/manager.py
+++ genweb/portlets/browser/manager.py
@@ -1,13 +1,21 @@
 from zope.component import adapts
 from zope.interface import Interface
+
 from zope.publisher.interfaces.browser import IBrowserView
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
 from plone.app.portlets.manager import ColumnPortletManagerRenderer
 from plone.app.portlets.browser.interfaces import IManageContextualPortletsView
 from plone.app.portlets.browser.editmanager import ContextualEditPortletManagerRenderer
+
+from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from genweb.portlets.browser.interfaces import IHomepagePortletManager
+
+from zope.annotation.interfaces import IAnnotations
+
+SPAN_KEY = 'genweb.portlets.span'
 
 
 class GenwebPortletRenderer(ColumnPortletManagerRenderer):
@@ -17,9 +25,35 @@ class GenwebPortletRenderer(ColumnPortletManagerRenderer):
     adapts(Interface, IDefaultBrowserLayer, IBrowserView, IHomepagePortletManager)
     template = ViewPageTemplateFile('templates/renderer.pt')
 
+    def get_span(self):
+        annotations = IAnnotations(self)
+        return annotations.setdefault(SPAN_KEY, False)
+
+    def prova(self):
+        #import ipdb;ipdb.set_trace()
+        pass
+
 
 class gwContextualEditPortletManagerRenderer(ContextualEditPortletManagerRenderer):
     """Render a portlet manager in edit mode for contextual portlets"""
     adapts(Interface, IDefaultBrowserLayer, IManageContextualPortletsView, IHomepagePortletManager)
 
     template = ViewPageTemplateFile('templates/edit-manager-contextual.pt')
+
+    def get_span(self):
+        annotations = IAnnotations(self)
+        self.span = annotations.setdefault(SPAN_KEY, False)
+        return self.span
+
+    def set_span(self, value):
+        annotations = IAnnotations(self)
+        annotations.setdefault(SPAN_KEY, value)
+        annotations[SPAN_KEY] = value
+
+    span = property(get_span, set_span)
+
+
+class setPortletHomeManagerSpan(BrowserView):
+    def __call__(self):
+        # import ipdb;ipdb.set_trace()
+        pass
